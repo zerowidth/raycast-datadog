8a7586cb47b12c29bd8d3c2a0de88f5416928c30
Nathan Witmer <nathan@zerowidth.com>
2024-01-07 22:42:02 -0700

    initialize the config file before opening it

--- src/config.tsx
+++ src/config.tsx
@@ -1,6 +1,7 @@
-import { configPath } from "./utils";
+import { configPath, initializeConfigFile } from "./utils";
 import open = require("open");
 import { closeMainWindow } from "@raycast/api";
 
+initializeConfigFile();
 open(configPath, { app: { name: "/Applications/Visual Studio Code.app" } });
 closeMainWindow();
